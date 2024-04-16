0c37aae94ad3917412c883033cb47d8183b07300
len.partington <len.partington@ausy.fr>
2024-04-16 17:26:49 +0200

    day 3

--- src/app/reservation-form/reservation-form.component.ts
+++ src/app/reservation-form/reservation-form.component.ts
@@ -34,10 +34,12 @@ constructor(
 
     let id =this.activatedRoute.snapshot.paramMap.get('id');
     if(id){
-      let reservation = this.reservationService.getReservation(id)
-      if(reservation){
+      this.reservationService.getReservation(id).subscribe( reservation => {
+        if(reservation){
           this.reservationForm.patchValue(reservation)
-      }
+        }
+      })
+      
       
     }
   }
@@ -50,9 +52,13 @@ constructor(
       let id =this.activatedRoute.snapshot.paramMap.get('id');
       if(id){
 
-        this.reservationService.updateReservation(id,reservation)
+        this.reservationService.updateReservation(id,reservation).subscribe( () => {
+          console.log("Update request processed");
+        })
       }else{
-        this.reservationService.addReservation(reservation)
+        this.reservationService.addReservation(reservation).subscribe( ( ) => {
+          console.log("Creation request processed");
+        })
       }
 
 
--- src/app/reservation/reservation.service.ts
+++ src/app/reservation/reservation.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Reservation } from '../models/reservation';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 
 @Injectable({
@@ -7,41 +9,34 @@ import { Reservation } from '../models/reservation';
 })
 export class ReservationService  {
 
+  private apiUrl="http://localhost:3001";
 
   private reservations:Reservation[]=[];
 
-  constructor(){//before onInit
-    let savedReservations=localStorage.getItem("reservations")
-    this.reservations=savedReservations?JSON.parse(savedReservations):[];
+  constructor(private http:HttpClient){}
+
+
+  getReservations():Observable<Reservation[]>{
+    return this.http.get<Reservation[]>(this.apiUrl+"/reservations");
   }
 
-
-  getReservations():Reservation[]{
-    return this.reservations;
+  getReservation(id:string):Observable<Reservation>{
+    return this.http.get<Reservation>(this.apiUrl+"/reservation/"+id);
   }
 
-  getReservation(id:string):Reservation|undefined{
-    return this.reservations.find(res=> res.id===id);
+  addReservation(reservation:Reservation):Observable<void>{
+    return this.http.post<void>(this.apiUrl+"/reservation/",reservation);
+
   }
 
-  addReservation(reservation:Reservation):void{
-    reservation.id=Date.now().toString();
+  deleteReservation(id:string):Observable<void>{
+    return this.http.delete<void>(this.apiUrl+"/reservation/"+id);
     
-    
-    this.reservations.push(reservation);
-    localStorage.setItem("reservations",JSON.stringify(this.reservations))
   }
 
-  deleteReservation(id:string):void{
-    let index =this.reservations.findIndex(res=> res.id ===id);
-    this.reservations.splice(index,1);
-    localStorage.setItem("reservations",JSON.stringify(this.reservations))
-  }
+  updateReservation(id:string,updatedReservation:Reservation):Observable<void>{
+    return this.http.put<void>(this.apiUrl+"/reservation/"+id,updatedReservation);
 
-  updateReservation(id:string,updatedReservation:Reservation):void{
-    let index = this.reservations.findIndex(res=> res.id===id);
-    this.reservations[index]=updatedReservation;
-    localStorage.setItem("reservations",JSON.stringify(this.reservations))
   }
 
 }
